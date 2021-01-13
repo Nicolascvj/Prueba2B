dddd75277ff5ed2d28cf8bc2fb9a1a50aaea69b4
Nicolas Cazco <ncazco@est.ups.edu.ec>
2021-01-12 22:23:29 -0500

    prueba

--- server/config/config.js
+++ server/config/config.js
@@ -12,13 +12,14 @@ process.env.NODE_ENV=process.env.NODE_ENV || 'dev';
 // Base de datos
 //==================================
 
-let urlDB;
+// let urlDB;
 
-if(process.env.NODE_ENV==='dev'){
-    urlDB="mongodb+srv://cocoa-user:zeCLBhugnx726afH@cluster0.l2gsu.mongodb.net/banco";
+// if(process.env.NODE_ENV==='dev'){
+//     urlDB="mongodb+srv://cocoa-user:zeCLBhugnx726afH@cluster0.l2gsu.mongodb.net/banco";
+//     //cocoa-user:zeCLBhugnx726afH
     
-}else{
-    urlDB="mongodb://localhost:27017/banco"
-}
+// }else{
+//     urlDB="mongodb://localhost:27017/banco"
+// }
 
-process.env.URLDB=urlDB;
+// process.env.URLDB=urlDB;
--- server/server.js
+++ server/server.js
@@ -16,14 +16,23 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/usuario'))
 
-mongoose.connect(
-    process.env.URLDB,
-    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
+// mongoose.connect(
+//     process.env.URLDB,
+//     { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
 
+//     (err, res) => {
+//         if (err) throw err;
+
+//         console.log('Base de datos ONLINE');
+//     }
+// );
+
+
+mongoose.connect(
+    "mongodb+srv://cocoa-user:zeCLBhugnx726afH@cluster0.l2gsu.mongodb.net/bancos?retryWrites=true&w=majority", { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
     (err, res) => {
         if (err) throw err;
-
-        console.log('Base de datos ONLINE');
+        console.log("Base de datos Online!");
     }
 );
 
