ff53cdbb7d0c1da2de36ea73312c6941979f732c
Nicolas Cazco <ncazco@est.ups.edu.ec>
2021-01-25 22:32:36 -0500

    Token 1

--- server/config/config.js
+++ server/config/config.js
@@ -28,3 +28,18 @@ if(process.env.NODE_ENV==='dev'){
 }
 
 process.env.URLDB=urlDB;
+
+
+// ===========================================
+//  Vencimiento del Token
+// ===========================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===========================================
+//  SEED de autenticación
+// ===========================================
+process.env.SEED = process.env.SEED || 'este-es-la-clave-en-dev';
